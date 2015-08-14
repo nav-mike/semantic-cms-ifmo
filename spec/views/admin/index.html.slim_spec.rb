27f553f4f6b25999ea793710aa5a1547389e88e2
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-08-14 15:53:33 +0000

    add first admin view spec

--- spec/views/admin/index.html.slim_spec.rb
+++ spec/views/admin/index.html.slim_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'admin/index.html.slim', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have h1 is "Dashboard"' do
+    render
+    expect(rendered).to have_content('Dashboard')
+  end
 end
