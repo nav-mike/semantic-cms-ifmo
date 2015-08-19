ef5dc172e8e6bfeffa877541bfed19a5d3014ae9
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-08-19 20:30:14 +0000

    add public graduate page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # Public pages controller class
 # @author M. Navrotskiy m.navrotskiy@gmail.com
 class PagesController < ApplicationController
-  layout 'public', only: %i(index abit student)
+  layout 'public', only: %i(index abit student graduate)
 
   def index
   end
