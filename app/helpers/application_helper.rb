dba1156e41544003c5ddc6cd7f32b6079e13cc18
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2015-08-19 16:15:12 +0000

    add public index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # Main application's herlpers module
 # @author M. Navrotskiy m.navrotskiy@gmail.com
 module ApplicationHelper
+  def title(page_title, options = {})
+    content_for(:title, page_title.to_s)
+    content_tag(:h1, page_title, options)
+  end
 end
--- spec/views/pages/index.html.slim_spec.rb
+++ spec/views/pages/index.html.slim_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'pages/index.html.slim', type: :view do
   it 'should have specific text on index page' do
     render
-    expect(rendered).to have_content 'Кафедра информатики и прикладной математики совместно с кафедрой ВТ обеспечивает подготовку'
+    expect(rendered).to have_content 'Кафедра информатики и прикладной математики совместо с кафедрой ВТ обеспечивает подготовку'
   end
 end
