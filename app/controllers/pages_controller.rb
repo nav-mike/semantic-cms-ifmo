9291105ac31495be0407c30d9c9c41b1c22bac92
M. Navrotskiy <m.navrotskiy@gmail.com>
2015-08-20 11:14:07 +0300

    add public contacts page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # Public pages controller class
 # @author M. Navrotskiy m.navrotskiy@gmail.com
 class PagesController < ApplicationController
-  layout 'public', only: %i(index abit student graduate history staff)
+  layout 'public', only: %i(index abit student graduate history staff contacts)
 
   def index
   end
