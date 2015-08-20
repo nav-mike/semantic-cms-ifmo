df976aef8d33e759776b69a93ef434c72bf23d8f
M. Navrotskiy <m.navrotskiy@gmail.com>
2015-08-20 10:37:14 +0300

    add public history page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # Public pages controller class
 # @author M. Navrotskiy m.navrotskiy@gmail.com
 class PagesController < ApplicationController
-  layout 'public', only: %i(index abit student graduate)
+  layout 'public', only: %i(index abit student graduate history)
 
   def index
   end
