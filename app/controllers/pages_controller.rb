7c70d059967f8bca17ed0d7d03daa13710bbf733
M. Navrotskiy <m.navrotskiy@gmail.com>
2015-08-20 10:43:22 +0300

    add public staff page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # Public pages controller class
 # @author M. Navrotskiy m.navrotskiy@gmail.com
 class PagesController < ApplicationController
-  layout 'public', only: %i(index abit student graduate history)
+  layout 'public', only: %i(index abit student graduate history staff)
 
   def index
   end
