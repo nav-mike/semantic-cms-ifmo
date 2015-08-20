e460f2a94c402761732c26c664be1d8c44245764
M. Navrotskiy <m.navrotskiy@gmail.com>
2015-08-20 11:48:19 +0300

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# User of cms
+# @author M. Navrotskiy m.navrotskiy@gmail.com
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
