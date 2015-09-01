47168155f7222d7ae1dc9540cee1b32a80735d42
M. Navrotskiy <m.navrotskiy@gmail.com>
2015-09-01 19:07:24 +0300

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+# Public users controller class
+# @author M. Navrotskiy m.navrotskiy@gmail.com
+class UsersController < AuthenticateController
+  layout 'admin'
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
