12f285fc0394ad24b6948508aa3fcaf1353565c0
M. Navrotskiy <m.navrotskiy@gmail.com>
2015-09-05 18:56:20 +0300

    add department_title and university_title settings values to layouts

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,4 +5,16 @@ class Setting < ActiveRecord::Base
     return unless can_removed
     delete
   end
+
+  def self.department_title
+    result = find_by(key: 'department_title').value
+    return 'Semantic CMS' if result.blank? || result == '-'
+    result
+  end
+
+  def self.university_title
+    result = find_by(key: 'university_title').value
+    return 'Semantic CMS' if result.blank? || result == '-'
+    result
+  end
 end
